83303700648a605fdcb1d8bcfdc0aacfdab86d94
gjwang <guijiewan@gmail.com>
2013-11-20 23:22:50 -0800

    add weihgt and port

--- conf.py
+++ conf.py
@@ -5,23 +5,25 @@ Created on 2013年10月29日
 
 @author: gjwang
 '''
-
+#region's name for short for nodeID
 nodeID = 'jx_ja'
+#post interval
 interval = 10
+#mountpoint for main disk
 mountpoint = '/data'
 device = None #device='/dev/sda9'
 netcard = 'eth0'
+#port for nginx in local host
+port = '80'
+#publishing server's name
 psname = 'ngnix'
-postUrl = 'http://127.0.0.1/getnodestate'
+#the host's url for posting xml file
+postUrl = 'http://xxx:port/lua'
+#
 wwwroot = '/data/ysten'
+#xml file for posting
 xmlfilename="NodeState.xml"
+#program's log 
 logfilename = "log/nodestate.log"
-
-
-REDIS_MEDIA_DATA_DB = 0
-
-REDIS_MD_CONFIG = {
-    "server": "127.0.0.1",
-    "port": 6379,
-    "db": REDIS_MEDIA_DATA_DB,
-}
+#所有主机整体配置的综合系数
+weight=1
